aee46f5b4907ba19145360f5e814930824d38650
vadimm <vadimm@saltedge.com>
2025-07-14 17:46:09 +0300

    Fix Zeitwerk compatibility in Railtie middleware registration

--- lib/trace_request_id/railtie.rb
+++ lib/trace_request_id/railtie.rb
@@ -18,7 +18,7 @@ class TraceRequestId
     def register_sidekiq_client_middleware
       Sidekiq.configure_client do |config|
         config.client_middleware do |chain|
-          chain.add SidekiqClientMiddleware
+          chain.add TraceRequestId::SidekiqClientMiddleware
         end
       end
     end
@@ -26,11 +26,11 @@ class TraceRequestId
     def register_sidekiq_server_middleware
       Sidekiq.configure_server do |config|
         config.client_middleware do |chain|
-          chain.add SidekiqClientMiddleware
+          chain.add TraceRequestId::SidekiqClientMiddleware
         end
 
         config.server_middleware do |chain|
-          chain.add SidekiqServerMiddleware
+          chain.add TraceRequestId::SidekiqServerMiddleware
         end
       end
     end
@@ -38,7 +38,7 @@ class TraceRequestId
     def register_sidekiq_test_middleware
       # attach Sidekiq middleware to Sidekiq:Testing
       Sidekiq::Testing.server_middleware do |chain|
-        chain.add SidekiqServerMiddleware
+        chain.add TraceRequestId::SidekiqServerMiddleware
       end
     end
   end
